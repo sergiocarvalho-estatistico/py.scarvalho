d02699d7febba37ee8b5bd2126ac73cf1cf244a7
sergiocarvalho-estatistico <sergio.icmc@gmail.com>
2019-08-16 23:04:47 -0300

    up all

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,9 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['recommonmark']
 
-
+source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
 
 # The suffix of source filenames.
 source_suffix = {
